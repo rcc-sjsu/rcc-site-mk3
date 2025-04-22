e33b5765f5c48593bbb599e93c77cdc8b65f8d93
Anton Clayton <anton.clayton@gmail.com>
2025-04-21 19:36:19 -0700

    static styling complete

--- app/(pages)/profile/page.tsx
+++ app/(pages)/profile/page.tsx
@@ -55,33 +55,31 @@ export default function Profile() {
       </section>
 
       {/* right side: requirements */}
-      <section className="text-center w-4xl bg-amber-50">
+      <section className={styles.right_section}>
         <h1 className={styles.requirements_title}>Requirements</h1>
-        <div className="flex flex-col items-center">
-          <div className="relative w-full max-w-md">
+        <div className="flex flex-col justify-center items-center">
+          <div className={styles.image_container}>
             <Image
               src="/images/profile-images/checkmark-circles.svg"
               alt="Requirements Steps"
               layout="responsive"
-              width={1200} // Adjust these values based on your image's actual width and desired aspect ratio
-              height={225}
+              width={540} // Adjust these values based on your image's actual width and desired aspect ratio
+              height={88}
               objectFit="contain"
             />
           </div>
-          <div className="flex justify-between w-full max-w-md mt-4">
-            {" "}
-            {/* Container for the text labels */}
-            <div className="text-center">
-              <p className="text-sm">Create an</p>
-              <p className="text-sm">Account</p>
+          <div className={styles.requirement_text_container}>
+            <div className="w-1/3 flex flex-col items-center justify-center">
+              <p className={styles.requirements_p}>Create an</p>
+              <p className={styles.requirements_p}>Account</p>
             </div>
-            <div className="text-center">
-              <p className="text-sm">Attend 1</p>
-              <p className="text-sm">Event</p>
+            <div className="w-1/3 flex flex-col items-center justify-center">
+              <p className={styles.requirements_p}>Attend 1</p>
+              <p className={styles.requirements_p}>Event</p>
             </div>
-            <div className="text-center">
-              <p className="text-sm">Attend 2</p>
-              <p className="text-sm">Events</p>
+            <div className="w-1/3 flex flex-col items-center justify-center">
+              <p className={styles.requirements_p}>Attend 2</p>
+              <p className={styles.requirements_p}>Events</p>
             </div>
           </div>
         </div>
