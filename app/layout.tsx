fa5408a6617c3f2f56ba702a62f3d9afe98a3f14
teelario <104655608+teelario@users.noreply.github.com>
2025-07-30 21:36:22 -0700

    Add font subset for vercel build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
 import type { Metadata } from "next";
-import { Inter, Nunito_Sans, Zilla_Slab } from 'next/font/google'
+import { Inter, Nunito_Sans, Zilla_Slab } from "next/font/google";
 import "./globals.css";
-import Header from './components/header';
+import Header from "./components/header";
 
 const inter = Inter({
-  variable: '--font-inter',
-  weight: '400'
-})
+  subsets: ["latin"],
+  variable: "--font-inter",
+  weight: "400",
+});
 
 const nunitoSans = Nunito_Sans({
-  variable: '--font-nunito-sans',
-  weight: '500'
-})
+  subsets: ["latin"],
+  variable: "--font-nunito-sans",
+  weight: "500",
+});
 
 const zillaSlab = Zilla_Slab({
-  variable: '--font-zilla-slab',
-  weight: '500'
-})
+  subsets: ["latin"],
+  variable: "--font-zilla-slab",
+  weight: "500",
+});
 
 export const metadata: Metadata = {
   title: "RCC Website",
@@ -30,7 +33,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${nunitoSans.variable} ${inter.variable} ${zillaSlab.variable}`}>
+      <body
+        className={`${nunitoSans.variable} ${inter.variable} ${zillaSlab.variable}`}
+      >
         <Header />
         {children}
       </body>
