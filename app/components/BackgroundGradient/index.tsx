4ab4961cf1cd2a2f1792b2c6938ca60a12df111a
ashley-rr <160376224+ashley-rr@users.noreply.github.com>
2025-08-04 11:59:40 -0700

    Minor rearranging of CSS file, deleted excess comments and unused props

--- app/components/BackgroundGradient/index.tsx
+++ app/components/BackgroundGradient/index.tsx
@@ -1,54 +1,21 @@
 import { gradientProps } from "./type";
-import "./BackgroundGradient.module.css"
+import styles from "./BackgroundGradient.module.css"
 
-export default function BackgroundGradient({className, secondClass, style, color, width, height}: gradientProps) {
-  
-  // const defaultStyle: React.CSSProperties = {
-  //   backgroundColor: color == "blue" ? "#E2E8FD" : "#DEB8FE",
-  //   width: "10rem",
-  //   height: "10rem",
-  //   opacity: color == "blue" ? "1" : "0.6",
-  //   filter: "blur(4rem)",
-  //   borderRadius: "50%",
-  //   zIndex: "-1"
-  // }
-
-  // const combinedStyle = {
-  //   ...defaultStyle,
-  //   ...style
-  // }
+export default function BackgroundGradient({className, color}: gradientProps) {
   
   return (
     
-    // <div 
-    // style={{
-    // width: width ? width + "rem" : "10rem",
-    //       height: height ? height + "rem" : "10rem",
-    // }}
-    // className={styles.gradientContainer}>
     <div
-    className={className}
-    style={{
-      display: "flex",
-      // width: width ? width + "rem" : "10rem",
-      // height: height ? height + "rem" : "10rem",
-    }}
+    className={`${className} ${styles.gradientContainer}`}
     >
       <div
-        className={secondClass}
+        className={styles.gradient}
         style={{
-          // combinedStyle
           backgroundColor: color == "blue" ? "#E2E8FD" : "#DEB8FE",
-          // width: width ? width + "rem" : "10rem",
-          // height: height ? height + "rem" : "10rem",
-          width: "100%",
-          height: "100%",
           opacity: color == "blue" ? "1" : "0.6",
-          filter: "blur(7rem)",
-          zIndex: "-1",
-          borderRadius: "50%"
         }}
       />
     </div>
+
   );
 }
--- app/components/BackgroundGradient/type.ts
+++ app/components/BackgroundGradient/type.ts
@@ -2,9 +2,5 @@ import React from 'react';
 
 export type gradientProps = {
   className?: string;
-  secondClass?: string;
-  style?: React.CSSProperties;
   color: "purple" | "blue";
-  width?: number;
-  height?: number;
 }
